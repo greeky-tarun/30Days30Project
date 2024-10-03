74fd7528de80a978f1505783e9cbf59d74084328
B. Tarun Kumar <btarunkumar7010@gmail.com>
2024-10-03 23:53:03 +0530

    game fucntion

---
+++ snake-game/script.js
@@ -0,0 +1,21 @@
+let dom_replay = document.querySelector("#replay");
+let dom_score = document.querySelector("#score");
+let dom_canvas = document.createElement("canvas"); 
+document.querySelector(".canvas").appendChild(dom_canvas); 
+let CTX = dom_canvas.getContext("2d");
+
+const W = (dom_canvas.width = 400);
+const H = (dom_canvas.height = 400);
+
+let snake,
+food,
+currentHue,
+cells=20,
+cellSize,
+isGameOver = false,
+score = 00,
+maxScore = window.localStorage.getItem("maxScore") || undefined,
+particles = [],
+splashingParticleCount = 20,
+cellsCount,
+requestID;
